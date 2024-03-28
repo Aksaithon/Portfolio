456c81bec8923d08250944d1ebaaea7e2981c501
Aksaithon <75464693+Aksaithon@users.noreply.github.com>
2024-03-28 14:39:29 +0530

    resposive navbar done

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,6 +1,17 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import { useEffect, useState } from "react";
 const Navbar = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove the event listener
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className={styles.navbar}>
       <div className={styles.logo_and_nav_links}>
@@ -24,29 +35,31 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className={styles.social_links}>
-        <a href="https://twitter.com/sharma_akarshak">
-          <img
-            src="/x-com.svg"
-            alt="twitter"
-            style={{ width: "20px", height: "20px" }}
-          />
-        </a>
-        <a href="https://github.com/Aksaithon">
-          <img
-            src="/github.svg"
-            alt="github"
-            style={{ width: "24px", height: "24px" }}
-          />
-        </a>
-        <a href="https://www.linkedin.com/in/akarshak-sharma/">
-          <img
-            src="/linkedin.svg"
-            alt="linkedin"
-            style={{ width: "24px", height: "24px" }}
-          />
-        </a>
-      </div>
+      {windowWidth > 768 && (
+        <div className={styles.social_links}>
+          <a href="https://twitter.com/sharma_akarshak">
+            <img
+              src="/x-com.svg"
+              alt="twitter"
+              style={{ width: "20px", height: "20px" }}
+            />
+          </a>
+          <a href="https://github.com/Aksaithon">
+            <img
+              src="/github.svg"
+              alt="github"
+              style={{ width: "24px", height: "24px" }}
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/akarshak-sharma/">
+            <img
+              src="/linkedin.svg"
+              alt="linkedin"
+              style={{ width: "24px", height: "24px" }}
+            />
+          </a>
+        </div>
+      )}
     </div>
   );
 };
